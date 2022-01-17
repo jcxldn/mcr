82e0ab4e1c1e73f57c8dfd8ece9dd7a9c5869a6d
James <jcahill773@gmail.com>
2022-01-17 21:04:40 +0000

    (ci): Make sure that containers start correctly
    Initial version

---
+++ .github/ci/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Usage: ./test.sh (tag) "<search pattern>"
+#    eg: ./test.sh jcxldn/minecraft-runner:waterfall-alpine "Listening on"
+
+echo "Found tag: '$1'"
+echo "Found search pattern: '$2'"
+
+
+echo "::group::Starting container"
+
+# Create empty test.log.txt
+# WSL refuses to create this if the extension is .log, so we're using log.txt instead
+echo > test.log.txt
+
+screen -L -Logfile test.log.txt -d -m -S container-test docker run -it --rm jcxldn/minecraft-runner:waterfall-alpine
+
+# Set flush time to 0secs
+screen -r container-test -X colon "logfile flush 0^M"
+
+tail -f -n0 test.log.txt &
+PID=$!
+
+tail -f -n0 test.log.txt | grep -qe "$2"
+
+if [ $? == 0 ]; then
+    echo -e "::endgroup::"
+    echo -e "::group::Stopping container"
+    screen -S container-test -X stuff "end\n"
+
+    # Wait for the server to stop
+    while screen -list | grep -q container-test
+    do
+        sleep 1
+    done
+    echo -e "::endgroup::"
+
+    echo "All finished!"
+    
+    echo "Killing leftover tail process $PID"
+    kill $PID
+
+fi
